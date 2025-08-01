7d2c31925b53b5fb808da6b3942442cbb6dcf30e
edcela <abanto.edcel@gmail.com>
2025-07-31 20:59:01 -0500

    Works with bloom and actually builds, removal of a ton of config stuff and then moved it into project-perry-descriptions, and removed tests.

--- bringup/launch/diffbot.launch.py
+++ bringup/launch/diffbot.launch.py
@@ -67,7 +67,7 @@ def generate_launch_description():
         ]
     )
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("ros2_control_demo_description"), "diffbot/rviz", "diffbot.rviz"]
+        [FindPackageShare("umrt-project-perry-description"), "rviz", "diffbot.rviz"]
     )
 
     control_node = Node(
